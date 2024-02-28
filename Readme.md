7f23797baa937e98a0625a5b66bd19806eb32483
julian <julian.parraperales@educa.madrid.org>
2024-02-28 13:12:02 +0100

    agregado test

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Ejercicio
+
+Dado el fichero de ```csv``` (fichero de datos separados por comas), se pide:
+
+- Analizar el tipo de datos que contiene el fichero
+- Diseñar una solución al problema:
+  - Qué clases necesitamos para almacenar la información del fichero?
+  - Qué estructura de paquetes debemos utilizar?
+- Implementar la solución para que dada una variable String con el contenido de ese fichero, devuelva los datos en objetos.
--- src/main/java/es/etg/daw/prog/stream/App.java
+++ src/main/java/es/etg/daw/prog/stream/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String cadena = "Nombre,Apellidos,DNI\n" + //
+                        "Julián,Parra,1a\n" + //
+                        "Ana,García,2b\n" + //
+                        "Pedro,López,2c\n" + //
+                        "Laura,Gómez,4d\n" + //
+                        "";
+        System.out.println(cadena);
     }
 }
